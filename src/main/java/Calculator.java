becff0068e4d8c99615a64f43f2f3cbc945e5312
Dhwanil Desai <dhwaniljames@gmail.com>
2026-02-25 16:25:05 -0800

    Fix Calculator class structure with multiply method

--- src/main/java/Calculator.java
+++ src/main/java/Calculator.java
@@ -6,7 +6,6 @@ public class Calculator {
     public int add(int a, int b) {
         return a + b;
     }
-}
 
     public int multiply(int a, int b) {
         return a + b; // bug: should be a * b
